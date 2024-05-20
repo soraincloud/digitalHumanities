3a89e551a9c8b5e3863994e327e3aebf7ae6ff40
soraincloud <soraincloud@gmail.com>
2024-05-20 23:44:52 +0800

    Update Home.vue

--- front_dig/src/views/Home.vue
+++ front_dig/src/views/Home.vue
@@ -2,15 +2,19 @@
     <div>
         云种植
         <div class="mb-4">
-    <el-button color="#a0cfff" :dark="isDark">加入购物车</el-button>
-    <el-button type="success">支付</el-button>
-    <el-button type="warning">收藏</el-button>
-    <el-button type="danger">取消</el-button>
-  </div>
+          <el-button color="#a0cfff" :dark="isDark">加入购物车</el-button>
+          <el-button type="success">支付</el-button>
+          <el-button type="warning">收藏</el-button>
+          <el-button type="danger">取消</el-button>
+        </div>
+        <div class="demo-image">
+            <el-image style="width: 100px; height: 100px" :src="url" fit="contain"/>
+        </div>
     </div>
 </template>
-<div class="demo-image">
-    <div v-for="fit in fits" :key="fit" class="block">
-      <span class="demonstration">{{ fit }}</span>
-      <el-image style="width: 100px; height: 100px" :src="url" :fit="fit" />
-    </div>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const url = require("@/assets/A.png")
+</script>

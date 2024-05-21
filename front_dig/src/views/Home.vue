4840cb921caa35aa8cb93509dfcf08c705b0816b
soraincloud <soraincloud@gmail.com>
2024-05-21 15:28:02 +0800

    Update Home.vue

--- front_dig/src/views/Home.vue
+++ front_dig/src/views/Home.vue
@@ -2,30 +2,28 @@
     <div>
         云种植
         <div class="mb-4">
-          <el-button color="#a0cfff" :dark="isDark">加入购物车</el-button>
-          <el-button type="success">支付</el-button>
+          <el-button color="#a0cfff">加入购物车</el-button>
+          <el-button type="success" @click="clickPay">支付</el-button>
           <el-button type="warning">收藏</el-button>
           <el-button type="danger">取消</el-button>
         </div>
         <div class="demo-image">
-            <el-image style="width: 500px; height: 500px" :src="url" fit="contain"/>
+            <el-image style="width: 500px; height: 500px" :src="urlA" fit="contain"></el-image>
         </div>
-        <img src="@/assets/B.png" alt="B" style="width: 500px; height= 500px">
-         <div class="block text-center">
-    <span class="demonstration">
-      Switch when indicator is hovered (default)
-    </span>
-    <el-carousel height="150px">
-      <el-carousel-item v-for="item in 5" :key="item">
-        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
-      </el-carousel-item>
-    </el-carousel>
-  </div>
+        <el-image v-if="isImageBShow" style="width: 500px; height: 500px" :src="urlB" fit="contain"></el-image>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-const url = require("@/assets/A.png")
+const urlA = require("@/assets/A.png") //A图片的位置
+const urlB = require("@/assets/B.png") //B图片的位置
+
+const isImageBShow = ref(false) //图片B是否显示 （默认不显示）
+
+const clickPay = () => //支付按钮按下
+{
+    isImageBShow.value = true //图片B显示
+}
 </script>

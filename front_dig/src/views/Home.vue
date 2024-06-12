36600fe88b7a1756ef324306f20ad26dcfc9f707
soraincloud <soraincloud@gmail.com>
2024-06-12 23:52:35 +0800

    update：修改页面布局 增加购买对话框

--- front_dig/src/views/Home.vue
+++ front_dig/src/views/Home.vue
@@ -1,55 +1,71 @@
 <template>
     <div>
-        <div class="mb-4">
-          <el-button color="#a0cfff" >装置1</el-button>
-        </div>
-        <div class="demo-image">
-            <el-image  style="width: 300px; height: 300px" :src="urlA" fit="contain"></el-image>
-        </div>
-        <div class="demo-image">
-            <el-image  style="width: 40px; height: 40px" :src="urlE" fit="contain"></el-image>
-            316 <el-font style="font-size: 40px;"></el-font>
-        </div>
-        <div class="mb-4">
-        <el-button type="danger">购买</el-button>
-        </div>
-        <div class="mb-4">   
-          <el-button color="#a0cfff" >装置2</el-button>
-        </div>
-        <div class="demo-image">
-            <el-image  style="width: 300px; height: 300px" :src="urlB" fit="contain"></el-image>
-        </div>
-        <div class="demo-image">
-            <el-image  style="width: 40px; height: 40px" :src="urlE" fit="contain"></el-image>
-            176
-        </div>
-        <div class="mb-4">
-        <el-button type="danger">购买</el-button>
-        </div>
-        <div class="mb-4">
-          <el-button color="#a0cfff" >装置3</el-button>
-        </div>
-        <div class="demo-image">
-            <el-image  style="width: 300px; height: 300px" :src="urlC" fit="contain"></el-image>
-        <div class="demo-image">
-            <el-image  style="width: 40px; height: 40px" :src="urlE" fit="contain"></el-image>
-            133
-        </div>
-        <div class="mb-4">
-        <el-button type="danger">购买</el-button>
-        </div>
-        </div>
-        <div class="mb-4">
-          <el-button color="#a0cfff" >装置4</el-button>
-        </div>
-        <div class="demo-image">
-            <el-image  style="width: 300px; height: 300px" :src="urlD" fit="contain"></el-image>
-        <div class="demo-image">
-            <el-image  style="width: 40px; height: 40px" :src="urlE" fit="contain"></el-image>
-            237
-        </div>
-        </div>
-        <el-button type="danger">购买</el-button>
+        <el-scrollbar :height="scrollHeight">
+            <el-card style="margin: 5px;">
+                <div>
+                    <el-button style="margin: 0px 0px 10px 10px;" color="#a0cfff" >装置1</el-button>
+                </div>
+                <div>
+                    <el-image  style="width: 300px; height: 300px" :src="urlA" fit="contain"></el-image>
+                </div>
+                <div style="display: flex;align-items: center;">
+                    <el-image style="width: 40px; height: 40px; margin-left: 10px;" :src="urlE" fit="contain"></el-image>
+                    <p style="margin-left: 10px; font-size: 20px;">316</p>
+                    <el-button style="margin-left:10px;" type="danger" @click="clickBuy(316)">购买</el-button>
+                </div>
+            </el-card>
+            <el-card style="margin: 5px;">
+                <div>
+                    <el-button style="margin: 0px 0px 10px 10px;" color="#a0cfff" >装置3</el-button>
+                </div>
+                <div>
+                    <el-image  style="width: 300px; height: 300px" :src="urlC" fit="contain"></el-image>
+                </div>
+                <div style="display: flex;align-items: center;">
+                    <el-image style="width: 40px; height: 40px; margin-left: 10px;" :src="urlE" fit="contain"></el-image>
+                    <p style="margin-left: 10px; font-size: 20px;">133</p>
+                    <el-button style="margin-left:10px;" type="danger" @click="clickBuy(133)">购买</el-button>
+                </div>
+            </el-card>
+            <el-card style="margin: 5px;">
+                <div>
+                    <el-button style="margin: 0px 0px 10px 10px;" color="#a0cfff" >装置2</el-button>
+                </div>
+                <div>
+                    <el-image  style="width: 300px; height: 300px" :src="urlB" fit="contain"></el-image>
+                </div>
+                <div style="display: flex;align-items: center;">
+                    <el-image style="width: 40px; height: 40px; margin-left: 10px;" :src="urlE" fit="contain"></el-image>
+                    <p style="margin-left: 10px; font-size: 20px;">176</p>
+                    <el-button style="margin-left:10px;" type="danger" @click="clickBuy(176)">购买</el-button>
+                </div>
+            </el-card>
+            <el-card style="margin: 5px;">
+                <div>
+                    <el-button style="margin: 0px 0px 10px 10px;" color="#a0cfff" >装置4</el-button>
+                </div>
+                <div>
+                    <el-image  style="width: 300px; height: 300px" :src="urlD" fit="contain"></el-image>
+                </div>
+                <div style="display: flex;align-items: center;">
+                    <el-image style="width: 40px; height: 40px; margin-left: 10px;" :src="urlE" fit="contain"></el-image>
+                    <p style="margin-left: 10px; font-size: 20px;">237</p>
+                    <el-button style="margin-left:10px;" type="danger" @click="clickBuy(237)">购买</el-button>
+                </div>
+            </el-card>
+        </el-scrollbar>
+        <el-dialog v-model="dialogVisible" title="确认订单">
+            <p>该订单将消费绿意值{{ greenCostNow }}</p>
+            <p></p>
+            <template #footer>
+                <div>
+                    <el-button @click="dialogVisible = false">取消</el-button>
+                    <el-button type="primary" @click="dialogVisible = false">
+                    确认
+                    </el-button>
+                </div>
+            </template>
+        </el-dialog>
     </div>
 </template>
 
@@ -61,5 +77,17 @@ const urlB = require("@/assets/B.jpg") //B图片的位置
 const urlC = require("@/assets/C.jpg") //C图片的位置
 const urlD = require("@/assets/D.jpg") //D图片的位置
 const urlE = require("@/assets/E.jpg") //E图片的位置
+
+const scrollHeight = ref(window.innerHeight - 100 + "px")
+
+const dialogVisible = ref(false)
+
+const greenCostNow = ref("0")
+
+const clickBuy = (num:any) =>
+{
+    greenCostNow.value = num
+    dialogVisible.value = true
+}
 
 </script>
